a9a67f0d543ab0ed7fad1fef86cc3bc5339f5b12
Jesse Stover <j.stover.r@gmail.com>
2026-03-17 11:51:24 -0500

    Removed faasr-functions subtree

--- faasr-functions/nullstart/README.md
+++
@@ -1,5 +0,0 @@
-# nullstart
-
-Simplest example of a FaaSr function that does nothing but log a short string. It takes no arguments.
-
-This can be used, for example, as a start function that triggers concurrent executions
--- faasr-functions/nullstart/nullstart.R
+++
@@ -1,6 +0,0 @@
-nullstart <- function() {
-    # Example function that does nothing but log a short string
-    # Can be used, for example, as a start function that triggers concurrent executions
-
-    faasr_log("nullstart successfully invoked")
-}
